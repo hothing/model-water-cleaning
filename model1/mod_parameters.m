7273f4c7f3901db84f8aaa143aca176b71a4110c
Mykhayl Puzanov <mykhayl.puzanov@inteco-atec.at>
2018-02-07 14:14:56 +0100

    The Matlab model at revision 0.1 has been added

---
+++ model1/mod_parameters.m
@@ -0,0 +1,12 @@
+% The model of  biochemical wtare cleaning
+V = 215; % [m3] volume of tank
+mu = 4; % [ml/(l*h)]
+mu_max = 6; % [ml/(l*h)]
+b = 0.05; % [ml/(l*h)]
+
+Y = 0.5; % [1] regeneration gain
+K = 5; % [mg/l] 
+Z = 0.3; % [1] 
+
+
+
